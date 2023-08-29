feca98ccfc8e92374668255d686c2155ae7f21c6
Javed Ansari <javedans2003@gmail.com>
2023-08-29 23:04:16 +0530

    Initial commit

---
+++ readme.md
@@ -0,0 +1,16 @@
+# Node.js TypeScript Template using SWC*
+
+- This is a Node.js TypeScript template using SWC.
+- To use this template, simply click on the "Use this template" button on GitHub and install dependencies using `pnpm i`.
+- Make sure to put all the code inside the `src` folder.
+
+- To run the TypeScript files, use the command 'npm run dev'.
+- To compile the TypeScript files to JavaScript files, use the command 'npm run build'.
+- To run the JavaScript files, use the command 'npm run prod'.
+
+- When importing from TypeScript files, make sure to add .js in the import statements, but not required for node_modules.
+
+- ```jsx
+  import fs from "fs"
+  import { test } from "./test.js";
+  ```
---
+++ src/index.ts
@@ -0,0 +1,17 @@
+/**
+ * This is a Node.js TypeScript template using SWC. 
+ * To use this template, simply click on the "Use this template" button on GitHub and install dependencies using `pnpm i`.
+ * Make sure to put all the code inside the `src` folder.
+ */
+
+console.log("Hi from index.ts");
+console.log("use the command 'npm run dev' to run the typescript files");
+console.log(
+  "use the command 'npm run build' to compile the typescript files to javascript files"
+);
+console.log("use the command 'npm run prod' to run the javascript files");
+console.log(
+  "make sure to add .js in the import statements while importing from ts files, not required for node_modules"
+);
+// import { test } from "./test.js";
+
