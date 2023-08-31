98cf62f9eefa75bbb8f7aaeeef4e9b089465e903
Javed Ansari <javedans2003@gmail.com>
2023-09-01 01:05:18 +0530

    node setup and basics

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,8 @@
-/**
- * This is a Node.js TypeScript template using SWC. 
- * To use this template, simply click on the "Use this template" button on GitHub and install dependencies using `pnpm i`.
- * Make sure to put all the code inside the `src` folder.
- */
+const express = require('express')
+const app = express()
 
-console.log("Hi from index.ts");
-console.log("use the command 'npm run dev' to run the typescript files");
-console.log(
-  "use the command 'npm run build' to compile the typescript files to javascript files"
-);
-console.log("use the command 'npm run prod' to run the javascript files");
-console.log(
-  "make sure to add .js in the import statements while importing from ts files, not required for node_modules"
-);
-// import { test } from "./test.js";
+app.get('/',(res,req)=>{
+  res.send("user page")
+})
 
+app.listen(3000)
